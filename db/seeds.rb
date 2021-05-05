2cb3cb19c4a454f83ce45fdab925d85886153b7d
gonta1026 <katano.original@gmail.com>
2021-05-05 20:44:07 +0900

    add: seedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+    article = Article.create(title: "Article ##{i}", body: "A Articleです.", status: "public")
+    5.times do |index|
+        article.comments.create(commenter: "私だー#111", body: "#111", status: "public")
+    end
+end
